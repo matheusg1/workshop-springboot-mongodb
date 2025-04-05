fcb78f7c23c0d73a59e9f6d62bcd9791a2f1911b
unknown <tecmatheusg@hotmail.com>
2025-04-05 14:29:47 -0300

    refactor: alterar retorno de users findAll para ResponseEntity

--- src/main/java/com/matheusg1/workshopmongo/resources/UserResource.java
+++ src/main/java/com/matheusg1/workshopmongo/resources/UserResource.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -17,11 +18,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserResource {
     
     @GetMapping()
-    public List<User> findAll() {
+    public ResponseEntity<List<User>> findAll() {
         List<User> list = new ArrayList<>();
         User maria = new User("1001", "Maria Brown", "maria@gmail.com"); 
         User alex = new User("1002", "Alex Green", "alex@gmail.com"); 
         list.addAll(Arrays.asList(maria, alex));
-        return list;
+        return ResponseEntity.ok().body(list);
     }
 }
